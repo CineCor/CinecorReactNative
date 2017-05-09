351fdc8e95c505915917dbfff591b70816361d7c
RTeran <rafaelterantorralbo@gmail.com>
2017-05-09 23:54:50 +0200

    Cinema types

---
+++ js/types/index.js
@@ -0,0 +1,6 @@
+// Cinemas
+export const FETCH_CINEMAS        = "FETCH_CINEMAS"
+export const CINEMAS_FETCH_FAILED = "CINEMAS_FETCH_FAILED"
+export const CINEMAS_RECEIVED     = "CINEMAS_RECEIVED"
+export const SAVE_TABS     				= "SAVE_TABS"
+export const CINEMA_SELECT        = "CINEMA_SELECT"
