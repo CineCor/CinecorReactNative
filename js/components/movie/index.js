7605549f2493a591d8eab64322c017c9e53a0ba0
RTeran <rafaelterantorralbo@gmail.com>
2017-05-09 23:55:19 +0200

    Movie component

---
+++ js/components/movie/index.js
@@ -0,0 +1,71 @@
+import React, {
+    Component, PropTypes as PT
+}                               from "react"
+import {
+    ListView, Text, View,
+    Image, TouchableOpacity
+}                               from "react-native"
+
+import myTheme                  from '../../themes/base-theme'
+import styles                   from "./style"
+
+class Movie extends Component {
+    static propTypes = {
+        id: PT.number,
+        image: PT.string,
+        title: PT.string,
+        hours: PT.array,
+        detail: PT.func
+    }
+
+    render() {
+        const { image, title, hours } = this.props
+
+
+        return (
+            <TouchableOpacity
+                onPress={() => this.props.detail()}
+                activeOpacity={OPACITY}
+                underlayColor={myTheme.primary}>
+                <View style={styles.containerList}>
+                    <Image
+                        source={{uri: image, cache: 'reload'}}
+                        style={styles.image}>
+                        <View style={styles.textIntoImage}>
+                            <Text
+                                style={styles.title}
+                                numberOfLines={1}>
+                                {title}
+                            </Text>
+                            <View style={styles.hours}>
+                                <Text
+                                    style={styles.hour}
+                                    numberOfLines={1}>
+                                    {
+                                        hours.map((hour, i) => {
+                                            const date = new Date(hour)
+                                            const hourFormated = date.getHours()
+                                            const minutesFormated = date.getHours()
+                                            const time = `${hourFormated}:${minutesFormated}   `
+                                            return time
+                                        })
+                                    }
+                                </Text>
+                            </View>
+                        </View>
+                    </Image>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+}
+
+const OPACITY = 0.75
+
+const mapStateToProps = (state) => ({
+    isFetching: state.cinemas.isFetching,
+    received: state.cinemas.received
+})
+
+
+export default Movie
---
+++ js/components/movie/style.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native';
+import myTheme        from '../../themes/base-theme';
+
+module.exports = StyleSheet.create({
+	container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  containerList: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: myTheme.light,
+    height: 100
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    height: 100,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  textIntoImage: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    textAlign: 'left',
+    color: myTheme.light
+  },
+  hours: {
+    flexDirection: 'row'
+  },
+  hour: {
+    fontSize: 16,
+    fontWeight: '600',
+    textAlign: 'left',
+    color: myTheme.light
+  }
+});
