e5d9214bc90867fab794c7b8e501eb59cfd9ced3
RTeran <rafaelterantorralbo@gmail.com>
2017-05-09 23:52:18 +0200

    Index component

--- js/components/index/index.js
+++ js/components/index/index.js
@@ -1,46 +1,38 @@
 
-import React, { Component }   from 'react';
-import Platform               from 'react-native';
-import { connect }            from 'react-redux';
-import I18n                   from 'react-native-i18n';
-import myTheme                from '../../themes/base-theme';
-import Tabs                   from '../tabs';
+import React, { Component }   from 'react'
+import Platform               from 'react-native'
+import { connect }            from 'react-redux'
+import I18n                   from 'react-native-i18n'
+import myTheme                from '../../themes/base-theme'
+import Tabs                   from '../tabs'
 import  {
   Container, Content,
   Text, Header, Button,
-  Body, Title,
-} from 'native-base';
+  Body, Title, Icon, Input,
+  InputGroup
+} 														from 'native-base'
 
 
 class Index extends Component {
-
-  constructor() {
-    super()
-    this.state = {
-      headerWidth: 0
-    }
-  }
-
-
-  getWidth(layout) {
-    const { width } = layout;
-    this.setState({ headerWidth: width })
-  }
 
   render() {
     return (
       <Container theme={ myTheme }>
         <Content>
-          <Header>
+          <Header
+            style={{backgroundColor: myTheme.primary}}
+            searchBar
+            rounded
+            iosBarStyle="light-content">
             <Body>
-              <Title>Cinecor</Title>
+              <Title style={{color: myTheme.light}}>Cinecor</Title>
             </Body>
           </Header>
           <Tabs />
         </Content>
       </Container>
-    );
+    )
   }
 }
 
-export default Index;
+export default Index
