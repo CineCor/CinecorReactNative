efa623424197af5fedf7b6d50843e7970379338c
RTeran <rafaelterantorralbo@gmail.com>
2017-06-08 18:41:18 +0200

    Fixed keyboard opened issue

--- js/components/index/index.js
+++ js/components/index/index.js
@@ -1,18 +1,20 @@
 
-import React, { Component }   from 'react'
-import { connect }            from 'react-redux'
-import I18n                   from 'react-native-i18n'
-import myTheme                from '../../themes/base-theme'
-import Tabs                   from '../tabs'
-import SearchBar              from '../searchBar'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import I18n from 'react-native-i18n'
+import { Keyboard } from 'react-native'
+import myTheme from '../../themes/base-theme'
+import Tabs from '../tabs'
+import SearchBar from '../searchBar'
 import  {
   Container, Content
-} 														from 'native-base'
+} from 'native-base'
 
 
 class Index extends Component {
 
   render() {
+		Keyboard.dismiss()
     return (
       <Container theme={ myTheme }>
         <SearchBar search={true} />
